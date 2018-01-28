599b9d1e55782c97f549cfe868d1f198a4fa0b42
Akshay Nair <phenax5@gmail.com>
2018-01-28 20:27:45 +0530

    validation added to users

--- src/components/users/UsersCtrlr.js
+++ src/components/users/UsersCtrlr.js
@@ -13,13 +13,23 @@ export default class Users extends Controller {
 
 		const user = new User(userData);
 
+		// Validate before saving
+		const res = user.check();
+		if (!res.isValid) {
+			delete res.isValid;
+			return ctx.body = { ...res, user: null };
+		}
+
 		return user.save()
-			.then(user => ctx.body = { user })
-			.catch(e => {
-				ctx.body = {
-					message: e.message,
-					status: 500,
-				};
+			.then(user => ctx.body = {
+				status: 200,
+				message: 'Saved',
+				user,
+			})
+			.catch(e => ctx.body = {
+				status: 500,
+				message: e.message,
+				user: null,
 			});
 	}
 
--- src/components/users/UsersModel.js
+++ src/components/users/UsersModel.js
@@ -1,4 +1,9 @@
 
+const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const NAME_INVALID = { isValid: false, status: 442, message: 'Name needs to be atleast 4 characters long' };
+const EMAIL_INVALID = { isValid: false, status: 443, message: 'Email address is invalid' };
+
 import mongoose from 'mongoose';
 
 export const schema = mongoose.Schema({
@@ -7,7 +12,18 @@ export const schema = mongoose.Schema({
 }, { collection: 'users' });
 
 Object.assign(schema.methods, {
-	// Entity methods
+	check() {
+
+		const user = this;
+
+		if (!user.name || user.name.length < 4)
+			return NAME_INVALID;
+
+		if (!user.email || !EMAIL_REGEX.test(user.email))
+			return EMAIL_INVALID;
+
+		return { isValid: true };
+	}
 });
 
 
